a5b18af26e696b5bf3cffa923522ab923805de0d
Rod Andrade <rodrigoandradebcc@gmail.com>
2021-02-07 22:10:30 -0300

    rota get exercises

--- src/routes/exercises.routes.ts
+++ src/routes/exercises.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import ExercisesRepository from '../repositories/ExercisesRepository';
 import CreateExerciseService from '../services/CreateExerciseService';
 
 const exercisesRouter = Router();
@@ -21,6 +23,13 @@ exercisesRouter.post('/', async (request, response) => {
     console.log(err);
     return response.status(400).json({ error: err.message });
   }
+});
+
+exercisesRouter.get('/', async (request, response) => {
+  const exercisesRepository = getCustomRepository(ExercisesRepository);
+  const exercises = await exercisesRepository.find();
+
+  return response.json(exercises);
 });
 
 export default exercisesRouter;
