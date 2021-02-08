ec695a53044ac8855344cab30bf52be4925a543f
Rod Andrade <rodrigoandradebcc@gmail.com>
2021-02-08 01:30:40 -0300

    Adicionando cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import express, { request, response } from 'express';
+import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
