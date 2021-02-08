f15fee00df7b927c4407227153d6b295c3bfa565
Rod Andrade <rodrigoandradebcc@gmail.com>
2021-02-08 01:31:18 -0300

    Retirando imports

--- src/routes/exercises.routes.ts
+++ src/routes/exercises.routes.ts
@@ -1,4 +1,4 @@
-import { request, response, Router } from 'express';
+import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 import ExercisesRepository from '../repositories/ExercisesRepository';
 import CreateExerciseService from '../services/CreateExerciseService';
